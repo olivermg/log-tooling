5760942a06df128f557271a244e955284a071d08
Oliver Wegner <void1976@gmail.com>
2019-05-16 15:23:08 +0200

    adding dependencies hiccup & zprint

--- log-reader/project.clj
+++ log-reader/project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [zprint "0.4.15"]]
   :main ^:skip-aot log-reader.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
