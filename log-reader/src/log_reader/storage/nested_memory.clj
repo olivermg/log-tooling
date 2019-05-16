38e78e9bb3ef6aee07bdd6463849446e6cc30d35
Oliver Wegner <void1976@gmail.com>
2019-05-16 15:23:28 +0200

    working on nested-memory

--- log-reader/src/log_reader/storage/nested_memory.clj
+++ log-reader/src/log_reader/storage/nested_memory.clj
@@ -1,69 +1,66 @@
 (ns log-reader.storage.nested-memory
   (:require [log-reader.nested-storage :as st]))
 
-(def ^:private +storage+ (atom {}))
+(defonce ^:private +nodes-map+ (atom {}))
 
-(defrecord Item [callinfo message children])
-(defrecord Callinfo [fn args])
-(defrecord Children [])
-(defrecord Message [message])
+(defn store-logline [nodes-map {:keys [trace] :as logline}]
+  (let [[path nodes-map] (reduce (fn [[path nodes-map] {:keys [id] :as trace-step}]
+                                   (let [path (conj path id)]
+                                     [(conj path :children)
+                                      (update-in nodes-map path #(assoc % :callinfo (dissoc trace-step :id)))]))
+                                 [[] nodes-map]
+                                 trace)
+        #_path      #_(->> (map :id trace)
+                       (interleave (repeat :children))
+                       rest
+                       vec)]
+    (assoc-in nodes-map (conj path :message) (dissoc logline :trace))))
 
-(defn store-entry [{:keys [trace] :as entry}]
-  (reduce (fn [path {:keys [id] :as trace-step}]
-            (let [path (conj path id)]
-              (swap! +storage+ update-in path #(assoc % :callinfo (dissoc trace-step :id)))
-              (conj path :children)))
-          [] trace)
-  (let [path (->> (map :id trace)
-                  (interleave (repeat :children))
-                  rest
-                  vec)]
-    (swap! +storage+ assoc-in (conj path :message) (dissoc entry :trace)))
-  @+storage+)
-
-(defn lookup [trace-ids]
+#_(defn lookup [nodes-map trace-ids]
   (let [path (-> (interleave (repeat :children) trace-ids)
                  vec)]
-    (some-> (get-in @+storage+ path)
+    (some-> (get-in nodes-map path)
             (update :children #(map (fn [[k v]]
                                       (conj (vec trace-ids) k))
                                     %)))))
 
-(defn all-data []
-  @+storage+)
+(defn sorted-data [nodes-map]
+  (->> (vals nodes-map)
+       (sort #(compare (some-> %1 :callinfo :time)
+                       (some-> %2 :callinfo :time)))))
+
+(defn traverse [f {:keys [children] :as node}]
+  (-> (update node :children
+              (fn [children]
+                (mapv (fn [[id child]]
+                        (traverse f child))
+                      children)))
+      f))
+
+(defn traverse-all [f nodes]
+  (map (partial traverse f) nodes))
 
 
 
-#_(do (store-entry {:trace [{:id :a :fn :afn} {:id :aa :fn :aafn}] :x "dataaa"})
-    (store-entry {:trace [{:id :b :fn :bfn}] :x "datab"})
-    (store-entry {:trace [{:id :a :fn :afn} {:id :ab :fn :abfn}] :x "dataab"})
-    (store-entry {:trace [{:id :a :fn :afn} {:id :ab :fn :abfn} {:id :aba :fn :abafn}] :x "dataaba"})
-    (store-entry {:trace [{:id :a :fn :afn} {:id :ab :fn :abfn} {:id :abb :fn :abbfn}] :x "dataabb"})
-    (clojure.pprint/pprint (all-data))
-    (println "=====")
-    #_(lookup [:a :ab])
-    (-> (clojure.walk/prewalk (fn [d]
-                                #_(if (map-entry? d)
-                                  (let [[k v] d]
-                                    (case k
-                                      :callinfo [:div.fn (:fn v)]
-                                      :message  [:div.message v]
-                                      :children d
-                                      d))
-                                  d)
-                                (cond
-                                  #_(map-entry? d) #_(do (println "MAPENTRY" d) d)
-                                  (and (map? d)
-                                       (contains? d :callinfo)) (do (println "ITEM" d)
-                                                                    (->> [:div.step
-                                                                          [:div.fn (some-> d :callinfo :fn)]
-                                                                          [:div.message (some-> d :message)]
-                                                                          (some-> d :children)]
-                                                                         (remove nil?)
-                                                                         vec))
-                                  (map? d) (do (println "CHILDREN" d)
-                                               (into [:div.children] (map val d)))
-
-                                  true d))
-                              (all-data))
-        clojure.pprint/pprint))
+#_(let [nodes-map (-> {}
+                    (store-logline {:trace [{:id :a :fn :afn :time 2} {:id :aa :fn :aafn :time 3}] :x "dataaa"})
+                    (store-logline {:trace [{:id :b :fn :bfn :time 1}] :x "datab"})
+                    (store-logline {:trace [{:id :a :fn :afn :time 3} {:id :ab :fn :abfn :time 4}] :x "dataab"})
+                    (store-logline {:trace [{:id :a :fn :afn :time 5} {:id :ab :fn :abfn :time 6} {:id :aba :fn :abafn :time 7}] :x "dataaba"})
+                    (store-logline {:trace [{:id :a :fn :afn :time 4} {:id :ab :fn :abfn :time 5} {:id :abb :fn :abbfn :time 6}] :x "dataabb"}))]
+  (clojure.pprint/pprint nodes-map)
+  (println "=====")
+  (clojure.pprint/pprint (sorted-data nodes-map))
+  (println "=====")
+  (->> (traverse-all (fn [{:keys [callinfo message children] :as node}]
+                       [:div.row
+                        (let [{:keys [fn time]} callinfo]
+                          [:div.callinfo
+                           [:div.fn fn]
+                           [:div.time time]])
+                        [:div.message
+                         [:pre message]]
+                        (into [:div.children] children)])
+                     (sorted-data nodes-map))
+       (into [:div])
+       zprint.core/zprint))
