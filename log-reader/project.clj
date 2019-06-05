9de577b37514712ce0136aaf4b7e52ae0c4abbd6
Oliver Wegner <void1976@gmail.com>
2019-06-05 14:48:28 +0200

    adding dependencies

--- log-reader/project.clj
+++ log-reader/project.clj
@@ -3,10 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [http-kit "2.3.0"]
+  :dependencies [[http-kit "2.3.0"]
                  [hiccup "1.0.5"]
-                 [zprint "0.4.15"]]
+                 [org.clojure/clojure "1.10.0"]
+                 [tick "0.4.14-alpha"]
+                 [zprint "0.4.15"]
+
+                 ;;; to avoid version conflicts:
+                 [org.clojure/tools.reader "1.2.2" :exclusions [org.clojure/clojure]]]
   :plugins [[io.taylorwood/lein-native-image "0.3.0"]]
   :main ^:skip-aot log-reader.core
   :target-path "target/%s"
@@ -17,4 +21,5 @@
                             "--no-fallback"
                             "--static"
                             "--verbose"]}
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :pedantic? :abort)
